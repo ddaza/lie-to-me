61f91c9be1f67354f99557eb0b44c5af97d3fb8f
andjosh <email@andjosh.com>
2016-07-30 14:58:15 -0500

    Add light sensor data

--- lie.js
+++ lie.js
@@ -18,22 +18,25 @@ function lie(keys) {
 
   function start(cb) {
     board.on('ready', function() {
-      const weather = new Shield({
-        variant: 'PHOTON',  // or ARDUINO
-        freq: 3000,         // Set the callback frequency to 1-second
-        elevation: 500      // Go to http://www.WhatIsMyElevation.com to get your current elevation
-      });
+        console.log('board ready');
+        const weather = new Shield({
+            variant: 'PHOTON',  // or ARDUINO
+            freq: 3000,         // Set the callback frequency to 1-second
+            elevation: 500      // Go to http://www.WhatIsMyElevation.com to get your current elevation
+        });
+        const led = new five.Led('A5');
+        const photoresistor = new five.Sensor({
+            pin: 'A4',
+            freq: 250
+        });
+        photoresistor.on('data', function onLight(data) {
+            console.log(data);
+        });
 
-      // The weather.on("data", callback) function invokes the anonymous callback function
-      // whenever the data from the sensor changes (no faster than every 25ms). The anonymous
-      // function is scoped to the object (e.g. this == the instance of Weather class object).
-      // setTimeout(
       weather.on('data', function () {
-        const led = new five.Led("A5");
         const wetdata = {
           deviceId: board.io.deviceId,
           location: '1871',
-          // celsius & fahrenheit are averages taken from both sensors on the shield
           celsius: this.celsius,
           fahrenheit: this.fahrenheit,
           relativeHumidity: this.relativeHumidity,
@@ -43,7 +46,7 @@ function lie(keys) {
         };
 
         console.log(wetdata);
-        if(wetdata.relativeHumidity > 40) {
+        if (wetdata.relativeHumidity > 40) {
             led.fadeIn();
         } else {
             led.fadeOut();
