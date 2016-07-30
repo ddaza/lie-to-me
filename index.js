aae15414329fb61caa95cdabfa06d9b05f35e4f0
ddaza <danielito@gmail.com>
2016-07-30 13:58:14 -0500

    extra setup

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 'use strict';
 const keys = require('./keys.json'); // need to add your keys
-const lie = require('./lie.js');
+const lie = require('./lie.js')(keys);
 
 (function () {
-  lie.init(keys);
+  lie.init();
   lie.start();
 })()
